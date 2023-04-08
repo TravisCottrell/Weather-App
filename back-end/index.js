df67e039fd4ff69109f3e29a688ddf5ff0d04c75
TravisCottrell <tcottre4@kent.edu>
2023-04-08 10:59:05 -0400

    added path direction for final build and uncommented for final build

--- back-end/index.js
+++ back-end/index.js
@@ -1,38 +1,42 @@
 require("dotenv").config();
 const express = require("express");
-//const sensor = require("node-dht-sensor");
+const path = require("path");
+const sensor = require("node-dht-sensor");
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+const reactBuild = path.join(__dirname, "../front-end", "dist");
+console.log(reactBuild);
+app.use(express.static(reactBuild));
 
-// app.get("/api/indoor", (req, res) => {
-//     sensor.read(22, 4, function (err, temperature, humidity) {
-//         temperature_f = temperature * (9 / 5) + 32;
+app.get("/api/indoor", (req, res) => {
+    sensor.read(22, 4, function (err, temperature, humidity) {
+        temperature_f = temperature * (9 / 5) + 32;
 
-//         if (!err) {
-//             res.json({ temperature: temperature_f, humidity: humidity });
-//             //res.json({ temperature: temperature_f, humidity: humidity.toFixed(1) })
-//         } else {
-//             res.status(500).json({ error: err });
-//         }
-//     });
-// });
+        if (!err) {
+            res.json({ temperature: temperature_f, humidity: humidity });
+            //res.json({ temperature: temperature_f, humidity: humidity.toFixed(1) })
+        } else {
+            res.status(500).json({ error: err });
+        }
+    });
+});
 
 //dev testing
-app.get("/api/indoor", async (req, res) => {
-    try {
-        const url = "http://192.168.50.82:3000/temperature";
-        const response = await fetch(url);
+// app.get("/api/indoor", async (req, res) => {
+//     try {
+//         const url = "http://192.168.50.82:3000/temperature";
+//         const response = await fetch(url);
 
-        const result = await response.json();
-        res.json({ data: result });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("could not get indoor data");
-    }
-});
+//         const result = await response.json();
+//         res.json({ data: result });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send("could not get indoor data");
+//     }
+// });
 
 app.get("/api/outdoor", async (req, res) => {
     try {
@@ -44,6 +48,10 @@ app.get("/api/outdoor", async (req, res) => {
         console.error(error);
         res.status(500).send("could not get outdoor data");
     }
+});
+
+app.get("*", async (req, res) => {
+    res.sendFile(path.join(reactBuild, "index.html"));
 });
 
 const port = process.env.PORT || 3005;
